a89d6711f60a8ca6fbc5ee0bedfd69ad1d5624da
Eric Fischer <ericf712@gmail.com>
2018-07-09 03:03:53 -0700

    updates graph logic to be specific for undirected graphs

--- random_contraction.py
+++ random_contraction.py
@@ -36,8 +36,8 @@ def preprocess_adj_list(filename):
 # output: object with vertex keys and their neighbors
 def graph_obj(arr_vertices):
     graph = {}
-    for i, val in enumerate(arr_vertices):
-        graph[val[0]] = val[1:]
+    for v in arr_vertices:
+        graph[v[0]] = v[1:]
     return graph
 
 
@@ -47,127 +47,131 @@ def create_graph(graph_obj):
     G = Graph()
     for v_key in graph_obj:
         v = Vertex(v_key)
-        for nbr_key in graph_obj[v_key]:
-            v.add_nbr(nbr_key)
+        for nbr_k in graph_obj[v_key]:
+            v.add_nbr(nbr_k)
         G.add_v(v)
     return G
 
 
-# Vertex class
+# Vertex class for undirected graphs
 class Vertex():
     def __init__(self, key):
-        self.key = key
-        self.nbrs = {}
+        self._key = key
+        self._nbrs = {}
 
     def __str__(self):
         return '{' + "'key': {}, 'nbrs': {}".format(
-            self.key,
-            self.nbrs
+            self._key,
+            self._nbrs
         ) + '}'
 
     def add_nbr(self, nbr_key, weight=1):
         if (nbr_key):
-            self.nbrs[nbr_key] = weight
+            self._nbrs[nbr_key] = weight
 
     def has_nbr(self, nbr_key):
-        return nbr_key in self.nbrs
+        return nbr_key in self._nbrs
 
     def get_nbr_keys(self):
-        return list(self.nbrs.keys())
+        return list(self._nbrs.keys())
 
     def remove_nbr(self, nbr_key):
-        if nbr_key in self.nbrs:
-            del self.nbrs[nbr_key]
+        if nbr_key in self._nbrs:
+            del self._nbrs[nbr_key]
 
-    def get_weight(self, nbr_key):
-        if nbr_key in self.nbrs:
-            return self.nbrs[nbr_key]
+    def get_e(self, nbr_key):
+        if nbr_key in self._nbrs:
+            return self._nbrs[nbr_key]
 
 
-# Graph class
-# Note: to maximize applications, add_edge, increase_edge, and remove_edge only add or remove an
-# edge for the 'from' vertex, and has_edge only checks the 'from' vertex.
+# Undirected graph class
 class Graph():
     def __init__(self):
-        self.vertices = {}
+        self._vertices = {}
 
     # 'x in graph' will use this containment logic
     def __contains__(self, key):
-        return key in self.vertices
+        return key in self._vertices
 
     # 'for x in graph' will use this iter() definition, where x is a vertex in an array
     def __iter__(self):
-        return iter(self.vertices.values())
+        return iter(self._vertices.values())
 
     def __str__(self):
         output = '\n{\n'
-        vertices = self.vertices.values()
+        vertices = self._vertices.values()
         for v in vertices:
-            graph_key = "{}".format(v.key)
+            graph_key = "{}".format(v._key)
             v_str = "\n   'key': {}, \n   'nbrs': {}".format(
-                v.key,
-                v.nbrs
+                v._key,
+                v._nbrs
             )
             output += ' ' + graph_key + ': {' + v_str + '\n },\n'
         return output + '}'
 
     def add_v(self, v):
         if v:
-            self.vertices[v.key] = v
-        # temporary logic
+            self._vertices[v._key] = v
         return self
 
     def get_v(self, key):
         try:
-            return self.vertices[key]
+            return self._vertices[key]
         except KeyError:
             return None
 
     def get_v_keys(self):
-        return list(self.vertices.keys())
+        return list(self._vertices.keys())
 
     # removes vertex as neighbor from all its neighbors, then deletes vertex
     def remove_v(self, key):
-        if key in self.vertices:
-            nbr_keys = self.vertices[key].get_nbr_keys()
+        if key in self._vertices:
+            nbr_keys = self._vertices[key].get_nbr_keys()
             for nbr_key in nbr_keys:
-                self.remove_edge(nbr_key, key)
-            del self.vertices[key]
-        # temporary logic
-        return self
+                self.remove_e(nbr_key, key)
+            del self._vertices[key]
 
-    def add_edge(self, from_key, to_key, weight=1):
-        if from_key not in self.vertices:
+    def add_e(self, from_key, to_key, weight=1):
+        if from_key not in self._vertices:
             self.add_v(Vertex(from_key))
-        if to_key not in self.vertices:
+        if to_key not in self._vertices:
             self.add_v(Vertex(to_key))
 
-        self.vertices[from_key].add_nbr(to_key, weight)
+        self._vertices[from_key].add_nbr(to_key, weight)
+        self._vertices[to_key].add_nbr(from_key, weight)
+
+    def get_e(self, from_key, to_key):
+        if from_key and to_key in self._vertices:
+            return self.get_v(from_key).get_e(to_key)
 
     # adds the weight for an edge if it exists already, with a default of 1
-    def increase_edge(self, from_key, to_key, weight=1):
-        if from_key not in self.vertices:
+    def increase_e(self, from_key, to_key, weight=1):
+        if from_key not in self._vertices:
             self.add_v(Vertex(from_key))
-        if to_key not in self.vertices:
+        if to_key not in self._vertices:
             self.add_v(Vertex(to_key))
 
-        weight_u_v = self.get_v(from_key).get_weight(to_key)
+        weight_u_v = self.get_v(from_key).get_e(to_key)
         new_weight_u_v = weight_u_v + weight if weight_u_v else weight
 
-        self.vertices[from_key].add_nbr(to_key, new_weight_u_v)
-        # temporary logic
-        return self
+        weight_v_u = self.get_v(to_key).get_e(from_key)
+        new_weight_v_u = weight_v_u + weight if weight_v_u else weight
 
-    def has_edge(self, from_key, to_key):
-        if from_key in self.vertices:
-            return self.vertices[from_key].has_nbr(to_key)
+        self._vertices[from_key].add_nbr(to_key, new_weight_u_v)
+        self._vertices[to_key].add_nbr(from_key, new_weight_v_u)
 
-    def remove_edge(self, from_key, to_key):
-        if from_key in self.vertices:
-            self.vertices[from_key].remove_nbr(to_key)
+    def has_e(self, from_key, to_key):
+        if from_key in self._vertices:
+            return self._vertices[from_key].has_nbr(to_key)
+
+    def remove_e(self, from_key, to_key):
+        if from_key in self._vertices:
+            self._vertices[from_key].remove_nbr(to_key)
+        if to_key in self._vertices:
+            self._vertices[to_key].remove_nbr(from_key)
 
     def for_each_v(self, cb):
-        for v in self.vertices:
+        for v in self._vertices:
             cb(v)
 
 
@@ -176,30 +180,30 @@ class Graph():
 def compile_edges(graph):
     edges = []
     v_keys = graph.get_v_keys()
-    for v_key in v_keys:
-        nbr_keys = graph.get_v(v_key).get_nbr_keys()
-        for nbr_key in nbr_keys:
-            edges.append([v_key, nbr_key])
+    for v_k in v_keys:
+        nbr_keys = graph.get_v(v_k).get_nbr_keys()
+        for nbr_k in nbr_keys:
+            edges.append([v_k, nbr_k])
     return edges
 
 
 # input: Graph object, array of all its edges
 # output: a random edge [u,v] that still exists in the graph
 def choose_edge(graph, edges):
-    random_edge = random.choice(edges)
-    u_key = random_edge[0]
-    v_key = random_edge[1]
-    while(not graph.has_edge(u_key, v_key)):
-        random_edge = random.choice(edges)
-        u_key = random_edge[0]
-        v_key = random_edge[1]
-    return random_edge
+    random_e = random.choice(edges)
+    u_key = random_e[0]
+    v_key = random_e[1]
+    while(not graph.has_e(u_key, v_key)):
+        random_e = random.choice(edges)
+        u_key = random_e[0]
+        v_key = random_e[1]
+    return random_e
 
 
 # input: array of all edges, v_key to be removed
 # output: array of all edges none of which contain v_key
-def remove_v_from_edges(edges, v_key):
-    for edge in edges:
+def remove_vertex_from_edges(edges, v_key):
+    for e in edges:
         # can exit iteration early if edges are sorted by edge[0], because v_key
         # will not be found again at edge[0] if larger. note it could be found at edge[1],
         # but exiting the for loop here has massive time benefits. and if we happen to choose
@@ -207,10 +211,10 @@ def remove_v_from_edges(edges, v_key):
         # (because we exited this for loop without deleting the edges in which it was at
         # edge[1]), choose_edge will discard this edge from the edges list and continue until
         # it finds a random edge that exists
-        if int(v_key) > int(edge[0]):
+        if int(v_key) > int(e[0]):
             break
-        if v_key in edge:
-            edges.remove(edge)
+        if v_key in e:
+            edges.remove(e)
     return edges
 
 
@@ -220,8 +224,8 @@ def count_edges(graph):
     edges = 0
     v = graph.get_v(graph.get_v_keys()[0])
     nbr_keys = v.get_nbr_keys()
-    for nbr_key in nbr_keys:
-        edges += v.get_weight(nbr_key)
+    for nbr_k in nbr_keys:
+        edges += v.get_e(nbr_k)
     return edges
 
 
@@ -251,74 +255,42 @@ def record_min_cut(graph_object):
 def random_contraction(G):
     edges = compile_edges(G)
     while len(G.get_v_keys()) > 2:
-        random_edge = choose_edge(G, edges)
-        u_key = random_edge[0]
-        v_key = random_edge[1]
-        u = G.get_v(u_key)
-        v = G.get_v(v_key)
+        random_e = choose_edge(G, edges)
+        u_k = random_e[0]
+        v_k = random_e[1]
+        u = G.get_v(u_k)
+        v = G.get_v(v_k)
 
         # Merge or contract u and v into a single vertex u (3 steps)
         # To prepare to delete v:
         # 1) for each v_nbr, a) add it to the neighbors of u, and b) add u to its neighbors
         v_nbr_keys = v.get_nbr_keys()
-        for v_nbr_key in v_nbr_keys:
-            # a) add v_nbr to neighbors of u, with an edge weight = [v,v_nbr] edge weight
+        for v_nbr_k in v_nbr_keys:
+            # a) add v_nbr and u as neighbors to each other with edge = v,v_nbr edge
             # (Note: self-loop created here when we eventually add u (also a v_nbr) as a
             # neighbor to itself, but it is removed in step 3)
-            weight_v_v_nbr = v.get_weight(v_nbr_key)
-            G.increase_edge(u_key, v_nbr_key, weight_v_v_nbr)
-            # b) add u to neighbors of v_nbr, with a [v_nbr,u] edge weight = same
-            # [u,v_nbr] edge weight just added
-            G.increase_edge(v_nbr_key, u_key, weight_v_v_nbr)
+            v_v_nbr_edge = v.get_e(v_nbr_k)
+            G.increase_e(u_k, v_nbr_k, v_v_nbr_edge)
             # append the new edges formed to list of edges
-            edges.append([u_key, v_nbr_key])
-            edges.append([v_nbr_key, u_key])
-            G.add_v(G.get_v(v_nbr_key))
+            edges.append([u_k, v_nbr_k])
+            edges.append([v_nbr_k, u_k])
 
         G.add_v(u)
         # 2) remove v from G, which removes it as anyones neighbor and deletes it
         # (remove from edges the edges v shared with neighbors and vice-versa)
-        G.remove_v(v_key)
-        edges = remove_v_from_edges(edges, v_key)
+        G.remove_v(v_k)
+        edges = remove_vertex_from_edges(edges, v_k)
         # 3) remove self-loop added in step 1a. u should not have itself listed in its neighbors
-        G.remove_edge(u_key, u_key)
+        G.remove_e(u_k, u_k)
 
     # return edges between final 2 vertices (will be equal for each vertex)
     return count_edges(G)
 
 
-# graph_object = {
-#     'a': {'b': 0, 'c': 0},
-#     'b': {'a': 0, 'c': 0, 'd': 0},
-#     'c': {'a': 0, 'b': 0, 'd': 0},
-#     'd': {'b': 0, 'c': 0}
-# }
 graph_object = preprocess_adj_list('random_contraction.txt')
 pprint.pprint(graph_object, width=260)
 
 start = time.time()
 best_min_cut = record_min_cut(graph_object)
 print('final best min cut:', best_min_cut)
-end = time.time()
-print('seconds:', end - start)
-
-# Play area:
-# p_graph = Graph()
-# p_graph.add_v(Vertex('apples'))
-# p_graph.add_v(Vertex('kittens'))
-# p_graph.add_edge('apples', 'kittens')
-# dogs = Vertex('dogs')
-# dogs.add_nbr('puppies')
-# dogs.remove_nbr('puppies')
-# print(p_graph)
-# print(p_graph.has_edge('apples', 'wolf'))
-# p_graph.add_edge('puppies', 'apples')
-# p_graph.add_edge('apples', 'puppies')
-# p_graph.add_edge('apples', 'kittens')
-# p_graph.add_edge('kittens', 'apples')
-# p_graph.add_edge('indiana', 'california')
-# p_graph.add_edge('california', 'indiana')
-# p_graph.remove_edge('indiana', 'california')
-# print('graph: ', p_graph)
-# p_graph.remove_v('puppies')
-# print('graph: ', p_graph)
+print('elapsed time:', time.time() - start)
